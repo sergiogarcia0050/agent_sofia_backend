0c2954014e5c3c533d819eeaf316cfa99a80969e
sergiogarcia0050 <segarcia@stone.com.co>
2025-11-22 10:22:36 -0500

    Inicial - FastAPI + Supabase

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+# Cargar archivo .env
+load_dotenv()
+
+# Leer variables
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
+
+app = FastAPI()
+
+# Crear cliente Supabase
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI + Supabase OK"}
+
+@app.get("/users")
+def get_users():
+    """Ejemplo: obtener datos de la tabla users"""
+    data = supabase.table("users").select("*").execute()
+    return data.data
